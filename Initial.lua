8dd8d60df04635ae483144c0bf511cb13f9e3b3e
Captain-Ozark <51975286+Captain-Ozark@users.noreply.github.com>
2019-12-25 17:23:02 -0500

    Updated File
    removed slash from in front of RC_App.lua

--- Initial.lua
+++ Initial.lua
@@ -1,4 +1,4 @@
 os.execute("wget https://raw.githubusercontent.com/Captain-Ozark/rc-controller/master/ButtonAPI.lua")
 os.execute("wget https://raw.githubusercontent.com/Captain-Ozark/rc-controller/master/RC_App.lua")
 package.loaded.buttonAPI = nil
-os.execute("/RC_App.lua")
+os.execute("RC_App.lua")
